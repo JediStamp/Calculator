98d50207958ea8f174fc2ee8526946d7858cf318
Jenne <jkmiklos@hotmail.com>
2021-04-25 12:51:16 -0400

    Fixed NumConverter
    NumConverter works for non-decimal bases from 1 to 36 (a-z)

---
+++ src/Calculations/NumConverter.java
@@ -0,0 +1,124 @@
+package Calculations;
+
+public class NumConverter {
+	
+	/**
+	 * Converts from any whole number base to base 10 - only for whole numbers
+	 * 
+	 * @param num - The input number as a String (to account for bases higher than 10)
+	 * @param base - The number system base of the original number
+	 * @return - Returns the decimal value, or -1 if the input contains 
+	 * a character other than alphanumeric
+	 */
+	public static int baseToDec( String numIn, int start_base ) {
+		int numOut = 0;
+		int valInt = 0;
+		String valStr;
+
+			// for each symbol
+			for ( int i = 0 ; i < numIn.length(); i++ ) {
+				valStr = (numIn.substring(numIn.length() - i - 1, numIn.length() - i ));
+				try {
+					// Get the number if it is a number
+					valInt = Integer.parseInt(valStr);
+				}
+				catch(NumberFormatException e) {
+					// If a letter, return the value of the letter					
+					if (valStr.matches("[A-Za-z]")) {
+						valInt = convAlphaToNum(numIn.charAt(numIn.length() - i - 1));
+					// Otherwise throw an exception	
+					} else {
+						System.out.println(e);
+						return -1;
+					}
+				}
+				//If number is equal to or larger than base, return -1
+				if ((valInt >= start_base)) {
+					return -1;
+				}
+				// Multiply by base to the power of place
+				numOut += valInt * (int) Math.pow(start_base, i);
+			}
+		return numOut;
+	}
+	
+	/**
+	 * Convert from base 10 to another base
+	 * 
+	 * @param numIn - integer number to be converted from decimal
+	 * @param base - any whole number
+	 * @return - String number in new base
+	 */
+	public static String decToBase( int numIn, int end_base ) {
+		String numOut = "";
+		int rem = 0;
+		// figure out how many loops to do
+		int len = getSize(numIn, end_base);
+
+		for (int i = 0; i < len; i++) {
+			rem = numIn % end_base;
+			// get next value
+			if (rem > 9) {
+				numOut = convNumToAlpha(rem) + numOut;
+			} else {
+				numOut = rem + numOut;
+			}
+			numIn = numIn / end_base;
+		}
+		return numOut;
+	}
+
+	public static String baseToBase (String numIn, int start_base, int end_base ) {
+		String numOut;
+		//System.out.println(baseToDec(numIn, start_base));
+		numOut = decToBase(baseToDec(numIn, start_base), end_base);
+		return numOut;
+	}
+	
+	/**
+	 * Converts a char to it's value (i.e. A = 10, B = 11)
+	 * Case insensitive
+	 * 
+	 * @param a - the char (letter) that needs to be converted to a number
+	 * @return - the number value of the char
+	 */
+ 	private static int convAlphaToNum(char a) {
+		//System.out.println("Numeric value is: " + Character.getNumericValue(a));
+		return Character.getNumericValue(a);
+	}
+
+ 	/**
+ 	 * Converts a number to it's equivalent alphabetic character
+ 	 * 
+ 	 * @param x - the number that needs to be converted to a letter
+ 	 * @return - char letter equivalent of number sent in (upper case)
+ 	 */
+ 	private static char convNumToAlpha(int x) {
+ 		if (x >= 10 && x <= 35) {
+ 	 		char a = (char) (x + 55);
+ 	 		//System.out.println("Char for num is: " + a);
+ 	 		return a;
+ 		} else {
+ 			return ' ';
+ 		}
+ 	}
+
+ 	/**
+ 	 * This calculates how long the output number will be
+ 	 * 
+ 	 * @param numIn - the number in decimal that needs to be converted
+ 	 * @param end_base - the base you are converting to
+ 	 * @return - the size of the new number (int)
+ 	 */
+ 	private static int getSize(int numIn, int end_base) {
+		int i = 0;
+		int j = numIn;
+		// figure out how many loops to do
+		while (j > 0) {
+			j = numIn / ((int) Math.pow(end_base, i));
+			i++;
+			System.out.println("j is: " + j + " i is " + i);
+		}
+		return i - 1;
+ 	}
+}
---
+++ src/Calculations/Quadratic.java
@@ -0,0 +1,27 @@
+package Calculations;
+
+public class Quadratic {
+	
+	/**
+	 * Performs Quadratic equation to get real roots (0, 1, or 2)
+	 * Form of ax^2 + bx = c = 0
+	 * 
+	 * @param a - x^2 coefficient (double) 
+	 * @param b - x coefficient (double) 
+	 * @param c - constant (double)
+	 * @return an array of doubles (size 2) with the roots. [Root1, Root2]
+	 * If there is only one root, it will be repeated [Root, Root]
+	 * If there are no real roots, values returned will be [NaN, NaN]
+	 */
+	public static Double[] calcQuadratic(double a, double b, double c) {
+		Double[] x = new Double[2];
+		double i = b*b - (4*a*c);
+
+		i = Math.sqrt(i);
+
+		x[0] =(-b + i)/(2.0*a);
+		x[1] = (-b - i)/(2.0*a);
+
+		return x;
+	}
+}
